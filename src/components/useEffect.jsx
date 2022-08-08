18cda29d0fb9a73f08b2f6d58540314c187f76ea
Parkwochang <whwh3560@gmail.com>
2022-08-08 23:46:51 +0900

    useEffect

--- src/components/useEffect.jsx
+++ src/components/useEffect.jsx
@@ -2,13 +2,21 @@ import { useEffect, useState } from "react";
 
 export default function UseEffect() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   const handleClick = () => {
     setCount(count + 1);
   };
 
-  //렌더링 될 때마다 매번 실행
-  useEffect(() => {});
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  //mount + item 변경될 때만 실행.
+  useEffect(() => {
+    console.log("count 변화");
+  }, [count]);
+  //dependency array
 
   return (
     <div>
@@ -16,6 +24,8 @@ export default function UseEffect() {
       <button style={{ marginLeft: "15px" }} onClick={handleClick}>
         Plus
       </button>
+      <input type="text" value={name} onChange={handleInputChange} />
+      <span>name: {name}</span>
     </div>
   );
 }
