74ae2e8446ccc9a60fc472ce25247499def72126
Parkwochang <whwh3560@gmail.com>
2022-08-09 23:44:49 +0900

    useEffect-clean up

--- src/components/useEffect.jsx
+++ src/components/useEffect.jsx
@@ -1,31 +1,13 @@
 import { useEffect, useState } from "react";
+import Timer from "./TImer";
 
 export default function UseEffect() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  const handleInputChange = (e) => {
-    setName(e.target.value);
-  };
-
-  //mount + item 변경될 때만 실행.
-  useEffect(() => {
-    console.log("count 변화");
-  }, [count]);
-  //dependency array
+  const [showTimer, setShowTimer] = useState(false);
 
   return (
     <div>
-      <span>count: {count}</span>
-      <button style={{ marginLeft: "15px" }} onClick={handleClick}>
-        Plus
-      </button>
-      <input type="text" value={name} onChange={handleInputChange} />
-      <span>name: {name}</span>
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
     </div>
   );
 }
